1764ab3ac0b99371fb0b270828d7779f8f4d0a86
Abhishek Nalwaya <Abhishek_Nalwaya@mckinsey.com>
2015-08-20 15:18:40 +0530

    sales code

---
+++ sales-tax1.js
@@ -0,0 +1,28 @@
+
+var seneca = require('seneca')();
+
+seneca.add({cmd: 'salestax'}, function(args, callback) {
+  var rate = args.rate;
+  var total = Math.round(args.net * (1 + rate));
+  callback(null, {total: total});
+});
+
+seneca.add({cmd: 'salestax', country: 'India'}, function(args, callback) {
+  var rate = 0.2;
+  seneca.act({cmd: 'salestax', rate: rate, net: args.net});
+});
+
+seneca.add({cmd: 'salestax', country: 'USA'}, function(args, callback) {
+  var rate = 0.3;
+  seneca.act({cmd: 'salestax', rate: rate, net: args.net});
+});
+
+seneca.act({cmd:'salestax',  country: 'USA', net: 100}, function(err, result) {
+  if (err) { return console.error(err); }
+  console.log(result.total);
+});
+
+seneca.act({cmd:'salestax', country: 'India', net: 100 }, function(err, result) {
+  if (err) { return console.error(err); }
+  console.log(result.total);
+});
